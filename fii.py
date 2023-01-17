6296c6b155f147998e64863ebba16fdff107b8f7
Fikri sinaga <107108003+fikriganteng-XD@users.noreply.github.com>
2023-01-17 21:08:58 +0700

    Add files via upload

---
+++ fii.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Mau Recode Yaaaa???
+# Silahkan Bree Izin Dulu Tapi Lah
+# Feri Pratama
+# CyberCarboon2
+#--------------------------[ MODULE ]---------------------
+import os,sys
+import widiya
+
+
+def __feri__():
+	widiya.main()
+	
+if __name__=="__main__":
+	__feri__()
